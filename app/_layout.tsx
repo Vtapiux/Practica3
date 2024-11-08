ee9c8803416b357e4f1411ad886b80f5535220b2
marco Graciano <marco.graciano@lasallistas.org.mx>
2024-11-08 17:14:10 -0600

    Esta es la buena

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,9 @@
 import { Stack } from "expo-router";
-import { MyContextProvider } from "./Context";
 
 export default function RootLayout() {
   return (
-    //Se anidan las pantallas en el mismo contexto
-    //MyContextProvider se utiliza para proveer el contexto a
-    //los elementos definidos por JSX
-    <MyContextProvider> 
-      <Stack>
-        <Stack.Screen name="index" options={{headerShown: false}} />
-        <Stack.Screen name="mainmenu" />
-        <Stack.Screen name="credits" />
-      </Stack>
-    </MyContextProvider>
-
+    <Stack>
+      <Stack.Screen name="index" options={{headerShown: false}} />
+    </Stack>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,12 @@
 import { Text, View, StyleSheet, 
-		TextInput, Pressable, Button
+		TextInput, Pressable
 } from "react-native";
-import * as Crypto from 'expo-crypto';
-import { useFonts } from "expo-font"; //para utilizar una fuente custom 
+import { useFonts } from "expo-font";
 import IconRocket from './iconrocket';
-import IconCar from './iconcar';
-import { Endpoints } from '../constants/Endpoint'; //se pone entre llaves porque es un objeto JavaScript (JSON)
-import { Link, router } from "expo-router" //para navegar entre ventanas
+import IconRobot from './robot';
+import { Endpoints } from "@/constants/Endpoints";
+import { useState } from 'react'
 
-import { useContext, useState } from "react";
-import { MyContext } from "./Context";
 
 //https://docs.expo.dev/develop/user-interface/fonts/
 //https://reactsvgicons.com/react-svg-icons-guide
@@ -21,106 +18,59 @@ export default function Index() {
 	  });
 
 	const [userValue, setUserValue] = useState('');
-	const [passValue, setPassValue] = useState('');	
-	const [failedLogin, setFailedLogin]=useState(false);
-	//Hace que las variables loginData y setLoginData correspondan con las que provee MyContext
-	const {loginData, setLoginData} = useContext(MyContext);
-	
-	const onPassValue = (pass: string) =>
-	{
-		setPassValue(pass);
-	}
-	
-	const onButtonRegister = async ()=>{
-		console.log("register");
-		router.replace('/register')
-	}
+	const [passValue, setPassValue] = useState('');
 
-	//BOTÓN DE LOGIN
 	const onButtonLogin = async ()=>
 	{
-		console.log("logging in");
+		console.log('logging in!');
 		//hacer la peticion de login
-		//console.log('requesting '+Endpoints.LOGIN);
-		
-		//hashear la contraseña
-		//await: función que tarda en regresar, no es síncrona
-
-		const passDigest = await Crypto.digestStringAsync(
-			Crypto.CryptoDigestAlgorithm.SHA256, passValue);
-		console.log(passDigest);
+		//console.log(Endpoints.LOGIN);
 
 		const form = new FormData();
 		form.append('token','code37');
 		form.append('user',userValue);
-		form.append('pass',passDigest);
-		
-		fetch(Endpoints.LOGIN, {
-			method: 'POST',
-			body:form,
+		form.append('pass', passValue);
+
+		fetch( Endpoints.LOGIN , {
+			method:'POST',
+			body:form
 		})
-		.then( response => response.json()
-		).then( data => {
-			if(!data.error && data.id)
-			{
-				setLoginData(data);
-				router.replace('/mainmenu'); //Reemplaza la pantalla principal por mainmenu una vez se logra login
-			}
-			else
-				setFailedLogin(true);
-		} )
-		.catch(err=>{ console.log(err) });
+		.then( response=>response.json())
+		.then( data => {console.log(data) })
+		.catch( err=>{console.log(err)});
 	}
 
 
   return (
     <View style={styles.container}>
-		<IconRocket width='100' height='100'></IconRocket>
-		<IconCar width='50' height='50'></IconCar> 
-		<Text style={styles.title}>ConnectMe</Text>
-		<Text >Conecta, Impacta, Destaca</Text>
+		<IconRocket width='150' height='150'></IconRocket>
+		<Text style={styles.title}>AppTitle</Text>
+		<Text >¡Te damos la bienvenida!</Text>
 		<View style={styles.inputfieldlabel}>
 			<Text >Usuario</Text>
-			<TextInput style={styles.input} onChangeText={setUserValue}></TextInput>
+			<TextInput style={styles.input} onChangeText={setUserValue}     secureTextEntry></TextInput>
 		</View>
-
-		<View style={styles.inputfieldlabel}>
-			<Text >Contraseña</Text>
-			<TextInput style={styles.input} 
-				secureTextEntry={true}
-				onChangeText={onPassValue}
-			></TextInput>
-		</View>
-
-		{failedLogin? (<Text style={styles.error}>Usuario o contraseña incorrectos</Text>):undefined}
-
-		<Pressable style={styles.botonconlogo} onPress={onButtonLogin}>
-			<IconCar width='32' height='32'></IconCar>
-			<Text>Log In</Text>
+		<Pressable style={styles.botonconlogo} onPress={onButtonLogin} >
+			<IconRobot width='32' height='32'></IconRobot>
+			<Text>Log in!</Text>
 		</Pressable>
-
 		<Text >¿No tienes una cuenta?</Text>
-
-		<Pressable style={styles.botonconlogo} onPress={onButtonRegister}>  
-			<IconRocket width='32' height='32'></IconRocket>
+		<Pressable style={styles.botonconlogo}>
+			<IconRobot width='32' height='32'></IconRobot>
 			<Text>Regístrate.</Text>
 		</Pressable>
 
-		<Link href="/mainmenu" asChild>
-			<Button title ="main"></Button>
-		</Link>
     </View>
 	
   );
 }
 
-
 const styles=StyleSheet.create(
 	{
 		container:{
 			flex: 1,
-			justifyContent: 'center',
-			alignItems: 'center',
+			justifyContent: "center",
+			alignItems: "center",
 		},
 		title:{
 			fontFamily:'poppins',
@@ -128,9 +78,9 @@ const styles=StyleSheet.create(
 		},
 		inputfieldlabel:
 		{
-			flexDirection:'row', //verticalLayout 
+			flexDirection:'row',
 			alignItems: 'center',
-			justifyContent: 'center',
+			justifyContent: 'flex-end',
 			width:'60%'
 		},
 		input: {
@@ -145,12 +95,11 @@ const styles=StyleSheet.create(
 			backgroundColor:'#F9D689',
 			flexDirection:'row',
 			alignItems: 'center',
-			padding:10,
-			borderRadius:5
-		},
-		error:{
-			color: "#F00",
-			padding: 5,
+			padding:5,
+			borderRadius:5,
+			borderColor:'#000',
+			borderWidth:2,
+			width:150
 		},
 		//#973131 #E0A75E #F9D689 #F5E7B2
 		
